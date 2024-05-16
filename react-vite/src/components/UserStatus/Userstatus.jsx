f8429bfb6fb091fda1357bcf09c37957d021cbbe
Nicholas Gould <nicgou@gmail.com>
2024-05-16 17:37:01 -0500

    integrated avatar with userstatus

--- react-vite/src/components/UserStatus/Userstatus.jsx
+++ react-vite/src/components/UserStatus/Userstatus.jsx
@@ -2,18 +2,21 @@
 import { useSelector, useDispatch } from "react-redux"
 import { useEffect, useState } from "react"
 import { thunkGetUserInfo } from "../../redux/user"
+import Avatar from "../Avatar/Avatar"
+
 
 
 const UserInfo = () => {
     const dispatch = useDispatch()
     const userSlice = useSelector(state => state.user)
 
+
     useEffect(() => {
         dispatch(thunkGetUserInfo());
     }, [dispatch])
 
     return <div className="userinfo_container">
-        <img id="profile_avatar" src={userSlice.avatar_url} alt='profile_avatar' />
+        < Avatar />
         <div id="userInfo">{userSlice.first_name} {userSlice.last_name} ({userSlice.username})</div>
     </div>
 
