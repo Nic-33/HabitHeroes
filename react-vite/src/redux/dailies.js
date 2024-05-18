401f116e0f48686443541c35b3acc8b2acda8b63
Manuel Tobal <mtobal93@gmail.com>
2024-05-18 15:34:43 -0400

    changed dalies thunk update

--- react-vite/src/redux/dailies.js
+++ react-vite/src/redux/dailies.js
@@ -64,7 +64,7 @@ export const thunkCreateDailies = (payload) => async (dispatch) => {
 
 export const thunkUpdateDailies = (payload, daily_id) => async (dispatch) => {
     const response = await fetch(`api/daily/${daily_id}`, {
-        method: "POST",
+        method: "PUT",
         headers: {
             'Content-Type': 'application/json'
         },
