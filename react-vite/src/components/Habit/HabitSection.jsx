5ebfa853c482b83ba23045340e2f7720dd2441da
Nicholas Gould <nicgou@gmail.com>
2024-05-18 12:38:34 -0500

    finished the create forms for todo and habits

--- react-vite/src/components/Habit/HabitSection.jsx
+++ react-vite/src/components/Habit/HabitSection.jsx
@@ -4,6 +4,9 @@ import { thunkGetHabits, thunkCreateHabits } from "../../redux/habits"
 import { useEffect, useState } from "react"
 import { months } from "../../data_utils/months"
 import './HabitSection.css'
+import CreateHabitForm from "../CreateHabitForm/CreateHabitForm"
+import OpenModalMenuItem from "../Navigation/OpenModalMenuItem.jsx";
+
 
 const HabitSection = () => {
     const dispatch = useDispatch()
@@ -30,12 +33,12 @@ const HabitSection = () => {
             // console.log(today.getMonth()+1)
             // console.log(today.getDate())
             // console.log(new Date(Date.now()))
-            
+
             // const date_to_reset = `${tomorrow.getFullYear()}-${tomorrow.getMonth()+1}-${tomorrow.getDate()+1}`
             const date_to_reset = months.calcTomorrow(months.length)
             console.log(date_to_reset)
             // const date_to_reset = new Date(Date.now())
-            const obj = { title, description, difficulty, frequency, date_to_reset,pos_count,neg_count }
+            const obj = { title, description, difficulty, frequency, date_to_reset, pos_count, neg_count }
             setHabitInput('');
             // habitSlice[3] = obj
             // const payload = JSON.stringify(obj)
@@ -54,11 +57,15 @@ const HabitSection = () => {
     return <div className="section_container">
         <h2>Habits</h2>
         <div className="section">
+            <li><OpenModalMenuItem
+                itemText="Create New Habit"
+                modalComponent={<CreateHabitForm />}
+            /></li>
             <input className="quick_input hoverable" type="text" value={habitInput} placeholder="Add a habit" onChange={(e) => handleHabitInput(e.target.value)} onKeyUpCapture={(e) => handleKeyPressEnter(e)}></input>
             {/* <button onClick={handleAddHabitClick}>add habit</button> */}
             <div >
                 {Object.keys(habitSlice).map(element => {
-                    return (<div  key={element}> <HabitInfo info_id={element} /></div>)
+                    return (<div key={element}> <HabitInfo info_id={element} /></div>)
                 })}
             </div>
         </div>
