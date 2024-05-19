0afac93808f12b36e1628fc8e3b10c76b95be9f3
Klevah101 <nsilobrown@hotmail.com>
2024-05-19 18:32:32 -0400

    more stuff

--- app/models/avatar.py
+++ app/models/avatar.py
@@ -9,7 +9,7 @@ class Avatar(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column(db.Integer(),  db.ForeignKey("users.id"),nullable=False,  unique=True)
+    user_id = db.Column(db.Integer(),  db.ForeignKey("users.id"),nullable=False)
     seed = db.Column(db.Integer(),nullable=False)
     eyes = db.Column(db.Integer(),nullable=False)
     mouth = db.Column(db.Integer(),nullable=False)
--- react-vite/src/components/Avatar/AvatarData.js
+++ react-vite/src/components/Avatar/AvatarData.js
@@ -1,5 +1,20 @@
 export const AvatarData = {
-'seed':['Socks','Mimi','Scooter','Cookie','Jack'],
-'eyes':['closed','closed2','crying','cute','glasses','love','pissed','plain','sad','shades','sleepClose','stars','tearDrop','wink','wink2'],
-'mouth':['cute','drip','faceMask','kissHeart','lilSmile','pissed','plain','sad','shout','shy','sick','smileLol','smileTeeth','tongueOut','wideSmile']
+    'seed': ['Socks', 'Mimi', 'Scooter', 'Cookie', 'Jack'],
+    'eyes': ['closed', 'closed2', 'crying', 'cute', 'glasses', 'love', 'pissed', 'plain', 'sad', 'shades', 'sleepClose', 'stars', 'tearDrop', 'wink', 'wink2'],
+    'mouth': ['cute', 'drip', 'faceMask', 'kissHeart', 'lilSmile', 'pissed', 'plain', 'sad', 'shout', 'shy', 'sick', 'smileLol', 'smileTeeth', 'tongueOut', 'wideSmile'],
+    seedIndex: (data, seed) => {
+        for (i = 0; i < data.seed.length; i++)
+            if (data.seed[i] === seed)
+                return data.seed[i]
+    },
+    eyesIndex: (data, eyes) => {
+        for (i = 0; i < data.eyes.length; i++)
+            if (data.eyes[i] === eyes)
+                return data.eyes[i]
+    },
+    mouthIndex: (data, mouth) => {
+        for (i = 0; i < data.mouth.length; i++)
+            if (data.mouth[i] === mouth)
+                return data.mouth[i]
+    },
 }
