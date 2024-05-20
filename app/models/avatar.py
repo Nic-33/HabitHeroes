c3959c22f5f46f4da02105111ba1c1cefa7e4ee2
Klevah101 <nsilobrown@hotmail.com>
2024-05-20 01:08:27 -0400

    m

--- app/models/avatar.py
+++ app/models/avatar.py
@@ -9,11 +9,7 @@ class Avatar(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer(), primary_key=True)
-<<<<<<< HEAD
-    user_id = db.Column(db.Integer(),  db.ForeignKey("users.id"),nullable=False)
-=======
     user_id = db.Column(db.Integer(),  db.ForeignKey(add_prefix_for_prod("users.id")),nullable=False)
->>>>>>> ed1ec09d1fd9b11d6a895944004e5f71cabdd045
     seed = db.Column(db.Integer(),nullable=False)
     eyes = db.Column(db.Integer(),nullable=False)
     mouth = db.Column(db.Integer(),nullable=False)
