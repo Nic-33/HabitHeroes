26a89179e597947dd82631a1dd199732b7838912
Manuel Tobal <mtobal93@gmail.com>
2024-05-20 00:34:02 -0400

    testing a new landing page

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,33 +1,113 @@
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal/SignupFormModal.jsx";
-import OpenModalMenuItem from "../Navigation/OpenModalMenuItem.jsx";
+import './LandingPage.css';
+import SignupFormPage from '../SignupFormPage';
+import SignupFormModal from '../SignupFormModal';
 import { useModal } from "../../context/Modal";
 
-import "./LandingPage.css";
-
-
 function LandingPage() {
-  const sessionUser = useSelector((state) => state.session.user);
+    const { setModalContent } = useModal();
+
+    return (<div className='landing-page'>
+        <div className="landing-page-ctn">
+            <div className='landing-sec-1'>
+                <div className='landing-header'>
+                    <img src="https://habitica.com/static/img/home-main@3x.23eeafe4.png" alt="motivating pixel art" />
+                    <h1>Motivate yourself to achieve your goals.</h1>
+                    <p>It's time to have fun when you get things done! Join over 4 people and improve your life one task at a time.</p>
+                </div>
+                <SignupFormPage/>
+
+            </div>
+
+            <div className='landing-sec-2'>
+                <div className='sec2MainCont'>
+                    <h2 className="ct">Gamify Your Life</h2>
+                    <p className="ct">Habit Heroes is a free habit-building and productivity app that treats your real life like a game. With in-game rewards and punishments to motivate you and a strong social network to inspire you, Habit Heroes can help you achieve your goals to become healthy, hard-working, and happy.</p>
+                </div>
+                <div className='hilight-ctn'>
+                    <div className='landing-hilight'>
+                        <img className="landing-img" src="https://habitica.com/static/img/track-habits@3x.7293d5cc.png" alt="images of task checked off"/>
+                        <div>
+                            <h3 className="ct">Track Your Habits and Goals</h3>
+                            <p className="ct">Stay accountable by tracking and managing your Habits, Daily goals, and To Do list with Habit Heroes’s easy-to-use mobile apps and web interface.</p>
+                        </div>
+                    </div>
+
+                    <div className='landing-hilight'>
+                        <img src='https://habitica.com/static/img/earn-rewards@3x.acaba17e.png' alt="mage surrounded by loot"/>
+                        <div>
+                            <h3 className="ct">Earn Rewards for Your Goals</h3>
+                            <p className="ct">Check off tasks to level up your Avatar and unlock in-game features such as battle armor, mysterious pets, magic skills, and even quests!</p>
+                        </div>
+                    </div>
+
+                    <div className='landing-hilight'>
+                        <img src="https://habitica.com/static/img/battle-monsters@3x.7cd8d3e0.png" alt="pixel people fight monster" />
+                        <div>
+                            <h3 className="ct">Battle Monsters with Friends</h3>
+                            <p className="ct">Fight monsters with other people! Use the Gold that you earn to buy in-game or custom rewards, like watching an episode of your favorite TV show.</p>
+                        </div>
+                    </div>
+                </div>
 
 
-  if (sessionUser) return <Navigate to="/" replace={true} />;
+            </div>
 
+            <div className='landing-sec-3'>
+                <h2 className="ct">Players Use Habit Heroes to Improve</h2>
 
-  return (
-    <>
-        <h1>landing page</h1>
-        <OpenModalMenuItem
-                itemText="Log In"
-                modalComponent={<LoginFormModal />}
-              />
-        <OpenModalMenuItem
-                itemText="Sign up"
-                modalComponent={<SignupFormModal />}
-              />
-    </>
-  )
+                <div className='hilight-ctn'>
+                    <div className='landing-hilight'>
+                        <img src="https://habitica.com/static/img/health-fitness@3x.9676cd74.png" alt="pixel person meditates"/>
+                        <div>
+                            <h3 className="ct">Health and Fitness</h3>
+                            <p className="ct">Never motivated to floss? Can't seem to get to the gym? Habit Heroes finally makes it fun to get healthy.</p>
+                        </div>
+                    </div>
+
+                    <div className='landing-hilight'>
+                        <img src='https://habitica.com/static/img/school-work@3x.d6eb1650.png' alt="pixel person at computer" />
+                        <div>
+                            <h3 className="ct">School and Work</h3>
+                            <p className="ct">Whether you're preparing a report for your teacher or your boss, it's easy to keep track of your progress as you tackle your toughest tasks.</p>
+                        </div>
+                    </div>
+
+                    <div className='landing-hilight'>
+                        <img src='https://habitica.com/static/img/much-more@3x.e3ed259a.png' alt="pixel person reads book"/>
+                        <div>
+                            <h3 className="ct">And much, much more!</h3>
+                            <p className="ct">Our fully customizable task list means that you can shape Habit Heroes to fit your personal goals. Work on creative projects, emphasize self-care, or pursue a different dream -- it's all up to you.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='landing-sec-4'>
+                <div>
+                    <img src='https://habitica.com/static/img/mobile-preview@3x.422fbec7.png' id="l-4-pic" alt = "app on phone"/>
+                </div>
+                <div className='sec4Content'>
+                    <h2 className="ct">Level Up Anywhere</h2>
+                    <p>Our app make it simple to keep track of your tasks on-the-go. Accomplish your goals with a single tap, no matter where you are.</p>
+
+                </div>
+            </div>
+
+            <div className='landing-sec-5'>
+                <h2 className='ct'>Join over 4 people having fun while accomplishing their goals!</h2>
+
+                <div onClick={() => setModalContent(<SignupFormModal />)} className="bottomJoin" >
+                    Join Habit Heroes Today
+                </div>
+
+            </div>
+
+            <div className='footer'>
+                {/* <img className='mountains' src='https://res.cloudinary.com/dzntryr5a/image/upload/v1694043036/mountains_drypau.png' />
+                <img className='mountains' src='https://habitica.com/static/img/midground_foreground_extended2.0fe8082c.png' /> */}
+            </div>
+        </div>
+    </div>);
 }
 
-export default LandingPage
+export default LandingPage;
