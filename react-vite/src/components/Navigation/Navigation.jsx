805082de35cd5c5207aef05eb5f5d4ecf82b3fe9
Manuel Tobal <mtobal93@gmail.com>
2024-07-08 23:07:19 -0400

    css on login modal, profile modal done. Changed landing page Login button

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -2,22 +2,39 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import logo from '../../images/habitHeroes.png'
+import logo from "../../images/habitHeroes.png";
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
-    const sessionUser = useSelector((state) => state.session.user);
-    let comingSoon = () => alert("Feature coming soon")
+    const user = useSelector((state) => state.session.user);
+    let comingSoon = () => alert("Feature coming soon");
 
     const loggedInLinks = (
         <>
-            <NavLink className="nav-links" exact to="/">Tasks</NavLink>
-            <NavLink className="nav-links" onClick={comingSoon}>Inventory</NavLink>
-            <NavLink className="nav-links" onClick={comingSoon}>Shops</NavLink>
-            <NavLink className="nav-links" onClick={comingSoon}>Party</NavLink>
-            <NavLink className="nav-links" onClick={comingSoon}>Group</NavLink>
-            <NavLink className="nav-links" onClick={comingSoon}>Challenges</NavLink>
-            <NavLink className="nav-links" onClick={comingSoon}>Help</NavLink>
+            <NavLink className="nav-links" exact to="/">
+                Tasks
+            </NavLink>
+            <NavLink className="nav-links" onClick={comingSoon}>
+                Inventory
+            </NavLink>
+            <NavLink className="nav-links" onClick={comingSoon}>
+                Shops
+            </NavLink>
+            <NavLink className="nav-links" onClick={comingSoon}>
+                Party
+            </NavLink>
+            <NavLink className="nav-links" onClick={comingSoon}>
+                Group
+            </NavLink>
+            <NavLink className="nav-links" onClick={comingSoon}>
+                Challenges
+            </NavLink>
+            <NavLink className="nav-links" onClick={comingSoon}>
+                Help
+            </NavLink>
         </>
     );
 
@@ -40,12 +57,26 @@ function Navigation({ isLoaded }) {
             <div className="nav-bar">
                 <div className="nav-links-ctn">
                     <NavLink exact to="/" className="nav-home">
-                        <img id='logo' src={logo} alt='home' />
+                        <img id="logo" src={logo} alt="home" />
                     </NavLink>
-                    {sessionUser ? loggedInLinks : loggedOutLinks}
+                    {user ? loggedInLinks : loggedOutLinks}
                 </div>
+
                 <div className="profile-button">
-                    <ProfileButton />
+                    {!user || user?.message === "user: null" ? (
+                        <>
+                            <div className="rightNavButtons">
+                                <OpenModalButton
+                                    buttonText="Log In"
+                                    modalComponent={<LoginFormModal />}
+                                />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <ProfileButton />
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
--- react-vite/src/components/Navigation/ProfileButton.jsx
+++ react-vite/src/components/Navigation/ProfileButton.jsx
@@ -1,82 +1,111 @@
 import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { FaRegUser } from "react-icons/fa6";
 import { thunkLogout } from "../../redux/session";
 import OpenModalMenuItem from "./OpenModalMenuItem";
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+import "./Navigation.css";
 
 function ProfileButton() {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  const user = useSelector((store) => store.session.user);
-  const ulRef = useRef();
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const user = useSelector((store) => store.session.user);
+    const ulRef = useRef();
 
-  const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
-    setShowMenu(!showMenu);
-  };
-
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = (e) => {
-      if (ulRef.current && !ulRef.current.contains(e.target)) {
-        setShowMenu(false);
-      }
+    const toggleMenu = (e) => {
+        e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+        setShowMenu(!showMenu);
     };
 
-    document.addEventListener("click", closeMenu);
+    useEffect(() => {
+        if (!showMenu) return;
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+        const closeMenu = (e) => {
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
 
-  const closeMenu = () => setShowMenu(false);
+        document.addEventListener("click", closeMenu);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(thunkLogout());
-    closeMenu();
-  };
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
 
-  return (
-    <>
-{/* 
+    const closeMenu = () => setShowMenu(false);
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(thunkLogout());
+        closeMenu();
+    };
+
+    return (
+        <>
+            {/* 
           <button className="menu" style={{ cursor: "pointer" }} onClick={toggleMenu}>
             <FaUserCircle /> */}
 
-          <button className="menu" style={{ cursor: "pointer", boxSizing:'border-box'}} onClick={toggleMenu}>
-            <FaRegUser />
-          </button>
+            {!user ||
+                (user?.message === "user: null" && (
+                    <div className="rightNavButtons">
+                        <OpenModalButton
+                            buttonText="Log In"
+                            modalComponent={<LoginFormModal />}
+                        />
+                        <OpenModalButton
+                            buttonText="Sign Up"
+                            modalComponent={<SignupFormModal />}
+                        />
+                    </div>
+                ))}
+            <div>
+                {user?.user_image_url ? (
+                    <img className="profileAvatar" src={user.user_image_url} />
+                ) : (
+                    <i className="fa-regular fa-user" onClick={toggleMenu} />
+                )}
+            </div>
 
-      {showMenu && (
-        <ul className={"profile-dropdown"} ref={ulRef}>
-          {user ? (
-            <>
-              <div>{user.username}</div>
-              <div>{user.email}</div>
-              <>
-                <button onClick={logout}>Log Out</button>
-              </>
-            </>
-          ) : (
-            <>
-              <OpenModalMenuItem
-                itemText="Log In"
-                onItemClick={closeMenu}
-                modalComponent={<LoginFormModal />}
-              />
-              <OpenModalMenuItem
-                itemText="Sign Up"
-                onItemClick={closeMenu}
-                modalComponent={<SignupFormModal />}
-              />
-            </>
-          )}
-        </ul>
-      )}
-    </>
-  );
+            {showMenu && (
+                <ul className={"profile-dropdown"} ref={ulRef}>
+                    {user || user?.message !== "user: null" ? (
+                        <>
+                            <div style={{ color: "black", fontSize: 24 }}>
+                                {user.username}
+                            </div>
+                            <div style={{ color: "black", fontSize: 24 }}>
+                                {user.email}
+                            </div>
+                            <>
+                                <button
+                                    className="login-signup-button"
+                                    onClick={logout}
+                                >
+                                    Log Out
+                                </button>
+                            </>
+                        </>
+                    ) : (
+                        <>
+                            <OpenModalMenuItem
+                                className="login-signup-button"
+                                itemText="Log In"
+                                onItemClick={closeMenu}
+                                modalComponent={<LoginFormModal />}
+                            />
+                            <OpenModalMenuItem
+                                className="login-signup-button"
+                                itemText="Sign Up"
+                                onItemClick={closeMenu}
+                                modalComponent={<SignupFormModal />}
+                            />
+                        </>
+                    )}
+                </ul>
+            )}
+        </>
+    );
 }
 
 export default ProfileButton;
